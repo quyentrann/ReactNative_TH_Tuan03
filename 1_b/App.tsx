c549a8e7ad7cb13e13c12364f549eba2bfbd693b
quyentrann <dangthiquyentran22@gmail.com>
2024-09-06 16:10:35 +0700

    screen 1_b

--- 1_b/App.tsx
+++ 1_b/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, Image, TouchableOpacity } from 'react-native';
+import { Text, View, Image, TouchableOpacity, TextInput } from 'react-native';
 
 const YourApp = () => {
   return (
@@ -13,7 +13,7 @@ const YourApp = () => {
         backgroundSize: 'cover',
         backgroundPosition: 'center',
       }}>
-      <View style={{ flex: 2, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={{ flex: 3, alignItems: 'center', justifyContent: 'center' }}>
         <Image
           source={{
             uri: 'https://res.cloudinary.com/dw7suzu56/image/upload/v1725508268/esepjewmvbfh7s70opcj.png',
@@ -31,7 +31,7 @@ const YourApp = () => {
           }}
         />
       </View>
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={{ flex: 2, justifyContent: 'center', alignItems: 'center' }}>
         <Text style={{ fontSize: 30, textAlign: 'center', fontWeight: 'bold' }}>
           FORGET {'\n'}PASSWORD
         </Text>
@@ -41,7 +41,6 @@ const YourApp = () => {
           flex: 1,
           justifyContent: 'flex-start',
           alignItems: 'center',
-          marginTop: 12,
           marginLeft: 12,
           marginRight: 12,
         }}>
@@ -49,24 +48,48 @@ const YourApp = () => {
           Provide your account’s email for which you want to reset your password
         </Text>
       </View>
-
+     <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          backgroundColor: '#C4C4C4',
+          height: 48,
+          width: 300,
+          borderRadius: 5,
+          paddingLeft: 10,
+        }}>
+        <Image
+          source={{
+            uri: 'https://cdn-icons-png.flaticon.com/512/561/561127.png', 
+          }}
+          style={{ width: 24, height: 24, marginRight: 10 }}
+        />
+        <TextInput
+          placeholder="Email"
+          style={{
+            flex: 1,
+            backgroundColor: '#C4C4C4',
+            height: '100%',
+          }}
+        />
+      </View>
       <View
         style={{
-          flex: 1,
+          flex: 2,
           flexDirection: 'row',
           alignItems: 'flex-start',
-          justifyContent: 'center',
+          justifyContent: 'flex-start',
         }}>
         <View style={{ justifyContent: 'center', alignItems: 'center' }}>
           <TouchableOpacity
             style={{
               height: 55,
-              width: 130,
+              width: 300,
               alignItems: 'center',
               justifyContent: 'center',
               backgroundColor: '#E3C000',
-              borderRadius: 12,
               marginRight: 10,
+              marginTop: 12
             }}>
             <Text style={{ fontWeight: 'bold', fontSize: 18 }}>NEXT</Text>
           </TouchableOpacity>
